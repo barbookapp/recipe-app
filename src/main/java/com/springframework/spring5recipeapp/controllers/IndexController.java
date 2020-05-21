9238d64e9afdc2045367180d0c4b2f7d6c27eb43
barbookapp <josh.orzano@gourmetgarnishllc.com>
2020-05-21 18:07:17 -0400

    Added bootstrap data and display on index.html

--- src/main/java/com/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,24 @@
 package com.springframework.spring5recipeapp.controllers;
 
+import com.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        System.out.println("Some message to say... 1234");
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
