0b4ca83109add5b90ebfc9d0514f32b00ec69de7
barbookapp <josh.orzano@gourmetgarnishllc.com>
2020-05-26 15:02:11 -0400

    Added project Lombok

--- src/main/java/com/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,12 +2,13 @@ package com.springframework.spring5recipeapp.services;
 
 import com.springframework.spring5recipeapp.domain.Recipe;
 import com.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
-
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
 
@@ -19,8 +20,9 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
-        Set<Recipe> recipeSet = new HashSet<>();
+        log.debug("I'm in the service");
 
+        Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
